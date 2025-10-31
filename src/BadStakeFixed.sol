d1a08d7292d4e9d6f474ad1c8190688d7052c3eb
Entreprenerd <alex@entreprenerd.xyz>
2025-10-31 08:25:01 +0100

    feat: founry

--- src/BadStakeFixed.sol
+++ src/BadStakeFixed.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import { ERC20 } from "solmate/tokens/ERC20.sol";
-import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
-import { Owned } from "solmate/auth/Owned.sol";
-import { ReentrancyGuard } from "solmate/utils/ReentrancyGuard.sol";
+import { ERC20 } from "./solmate/tokens/ERC20.sol";
+import { SafeTransferLib } from "./solmate/utils/SafeTransferLib.sol";
+import { Owned } from "./solmate/auth/Owned.sol";
+import { ReentrancyGuard } from "./solmate/utils/ReentrancyGuard.sol";
 
 contract BadStakeFixed is Owned, ReentrancyGuard {
     using SafeTransferLib for ERC20;
